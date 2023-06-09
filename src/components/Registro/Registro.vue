f4f25f1e4d544143422d469ef3337350361ac98d
Yan-Fonseca <66083422+Yan-Fonseca@users.noreply.github.com>
2023-06-09 19:01:08 -0300

    Adição da estrutura view do registro e do componente registro

---
+++ src/components/Registro/Registro.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, pariatur. In provident numquam molestiae ducimus. Iusto eaque nobis dolorem corporis laborum porro aliquid exercitationem sapiente, optio quaerat quam beatae rerum!</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'RegistroComp'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/RegistroView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="corpo">
+        <RegistroComp/>
+    </div>
+</template>
+
+<script>
+import RegistroComp from '@/components/Registro/Registro.vue'
+
+export default {
+    name: 'RegistroVue',
+    components: {
+        RegistroComp
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
