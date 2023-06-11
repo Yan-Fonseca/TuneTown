d45296ae7a390a4764a9b8ef6d5db62e81bcc454
Yan-Fonseca <66083422+Yan-Fonseca@users.noreply.github.com>
2023-06-11 19:30:23 -0300

    adicionando a div class registro-container e melhorando o css RegistroView

--- src/views/RegistroView.vue
+++ src/views/RegistroView.vue
@@ -1,27 +1,39 @@
 <template>
     <div class="corpo">
-        <RegistroComp/>
+      <div class="registro-container">
+        <RegistroComp />
+      </div>
     </div>
-</template>
-
-<script>
-import RegistroComp from '@/components/Registro/Registro.vue'
-
-export default {
+  </template>
+  
+  <script>
+  import RegistroComp from '@/components/Registro/Registro.vue'
+  
+  export default {
     name: 'RegistroVue',
     components: {
-        RegistroComp
+      RegistroComp
     }
-}
-</script>
-
-<style scoped>
-    .corpo {
-        background-image: url("../assets/imgs/fundo_registro.png");
-        background-size: cover;
-        background-position: center;
-        background-repeat: no-repeat;
-        min-height: 100vh;
-        margin: 0px;
-    }
-</style>
+  }
+  </script>
+  
+  <style scoped>
+  .corpo {
+    background-image: url("../assets/imgs/fundo_registro.png");
+    background-size: cover;
+    background-position: center;
+    min-height: 100vh;
+    margin: 0;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    background-repeat: no-repeat;
+    background-color: transparent;
+  }
+  
+  .registro-container {
+    margin-right: 50px;
+    background-color: rgba(0, 3, 22, 1);
+  }
+  </style>
+  
