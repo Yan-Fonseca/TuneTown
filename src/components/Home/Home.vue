498010c5e04209a73ec0876b4108eaa4a96719f6
Yan-Fonseca <66083422+Yan-Fonseca@users.noreply.github.com>
2023-06-12 22:52:23 -0300

    Estrutura de pacotes e arquivos da Home Page

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="Home">
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomePage'
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld/>
+    <HomePage/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HomePage from '@/components/Home/Home.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HomePage
   }
 }
 </script>
