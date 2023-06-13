096235327beb9249d283517c2d3d8f7828e55753
Yan-Fonseca <66083422+Yan-Fonseca@users.noreply.github.com>
2023-06-13 19:33:32 -0300

    Adição da aba de resultados

--- src/components/Busca/Resultados.vue
+++ src/components/Busca/Resultados.vue
@@ -1,9 +1,38 @@
 <template>
-    
+    <div class="container">
+        <h1>Resultados:</h1>
+        <div class="resultados">
+            <perfilCard/>
+            <perfilCard/>
+            <perfilCard/>
+            <perfilCard/>
+            <perfilCard/>
+            <perfilCard/>
+            <perfilCard/>
+            <perfilCard/>
+        </div>
+    </div>
 </template>
 
 <script>
+import perfilCard from '../common/perfilCard.vue';
+
 export default {
-    name: 'ResultadosBusca'
+    name: 'ResultadosBusca',
+    components: {
+        perfilCard
+    }
 }
 </script>
+
+<style scoped>
+    .container {
+        background-color: rgba(29, 36, 45, 0.84);
+        color: white;
+        border-radius: 20px;
+    }
+
+    h1 {
+        font-size: 47px;
+    }
+</style>
--- src/views/BuscaView.vue
+++ src/views/BuscaView.vue
@@ -2,18 +2,21 @@
     <div class="container">
         <BuscaPerfil/>
         <FiltroBusca/>
+        <ResultadosBusca/>
     </div>
 </template>
 
 <script>
     import BuscaPerfil from "../components/Busca/Busca.vue"
     import FiltroBusca from "../components/Busca/Filtro.vue"
+    import ResultadosBusca from "@/components/Busca/Resultados.vue";
 
     export default {
         name: 'BuscaView',
         components: {
             BuscaPerfil,
-            FiltroBusca
+            FiltroBusca,
+            ResultadosBusca
         }
     }
 </script>
