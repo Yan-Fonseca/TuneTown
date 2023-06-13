848b02bc82a4ef0cd881c7de0055cdf6d102a9d5
Yan-Fonseca <66083422+Yan-Fonseca@users.noreply.github.com>
2023-06-12 22:19:54 -0300

    inicio do portfolio: Adição do campo de entrada de descrição e upload de arquivos

--- src/components/Perfil/Portfolio.vue
+++ src/components/Perfil/Portfolio.vue
@@ -1,9 +1,41 @@
 <template>
-    
+    <div class="portfolio">
+        <div class="novo-trabalho">
+            <div class="descricao">
+                <h3>Trabalho</h3>
+                <textarea v-model="texto" rows="6"></textarea>
+            </div>
+        </div>
+        <div class="upload">
+            <input type="file" accept="image/*, video/*" @change="handleFileUpload">
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'Portfolio'
+    name: 'PortfolioMusic',
+    data() {
+        return {
+        texto: ""
+        };
+    },
+    methods: {
+    handleFileUpload(event) {
+      const file = event.target.files[0];
+      // ações futuras
+      console.log("Arquivo selecionado:", file);
+    }
+  }
 }
 </script>
+
+<style>
+textarea {
+  width: 100%;
+  padding: 10px;
+  font-size: 14px;
+  border: 1px solid #ccc;
+  resize: vertical;
+}
+</style>
--- src/views/PerfilView.vue
+++ src/views/PerfilView.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="perfil">
     <BasicInfo/>
+    <PortfolioMusic/>
   </div>
 </template>
 
 <script>
 import BasicInfo from '@/components/Perfil/BasicInfo.vue';
+import PortfolioMusic from '@/components/Perfil/Portfolio.vue';
 
 export default {
   name: 'PerfilView',
   components: {
-    BasicInfo
+    BasicInfo,
+    PortfolioMusic
   }
 }
 </script>
