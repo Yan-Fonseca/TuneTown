366f96bf258c075f962ea6edeef2f23bd38181df
Yan-Fonseca <66083422+Yan-Fonseca@users.noreply.github.com>
2023-06-13 16:27:25 -0300

    Alinhando componentes do card do Perfil

--- src/components/common/perfilCard.vue
+++ src/components/common/perfilCard.vue
@@ -1,32 +1,45 @@
 <template>
     <div class="perfil-card">
-        <img src="../../assets/imgs/perfil.png" alt="foto de perfil">
-        <div class="data">
-            <p>Nome</p>
-            <button class="perfil-btn">Perfil</button>
-            <p>Avaliação: x.x/5.0</p>
-        </div>
+      <img src="../../assets/imgs/perfil.png" alt="foto de perfil">
+      <div class="data">
+        <p class="nome">Nome</p>
+        <button class="perfil-btn">Perfil</button>
+        <p class="avaliacao">Avaliação: x.x/5.0</p>
+      </div>
     </div>
-</template>
-
-<script>
-    export default {
-        name: 'PerfilCard'
-    }
-</script>
-
-<style scoped>
-    .perfil-card {
-        width: 187px;
-        height: 241px;
-        border-radius: 15px;
-        background-color: rgba(29, 36, 45, 0.84);
-        color: white;
-    }
-
-    .perfil-btn {
-        border-radius: 7px;
-        color:black;
-        background-color: rgba(56, 123, 223, 1);
-    }
-</style>
+  </template>
+  
+  <script>
+  export default {
+    name: 'PerfilCard'
+  }
+  </script>
+  
+  <style scoped>
+  .perfil-card {
+    width: 187px;
+    height: 241px;
+    border-radius: 15px;
+    background-color: rgba(29, 36, 45, 0.84);
+    color: white;
+    display: flex; /* Adicionado para usar flexbox */
+    flex-direction: column; /* Alinha os itens verticalmente */
+    justify-content: center; /* Centraliza verticalmente */
+    align-items: center; /* Centraliza horizontalmente */
+  }
+  
+  .data {
+    text-align: center; /* Alinha o conteúdo ao centro horizontalmente */
+  }
+  
+  .nome, .perfil-btn, .avaliacao {
+    margin: 5px; /* Adiciona um espaço entre os elementos */
+  }
+  
+  .perfil-btn {
+    border-radius: 7px;
+    color: black;
+    background-color: rgba(56, 123, 223, 1);
+  }
+  </style>
+  
