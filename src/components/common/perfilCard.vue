79b9340024059349f21230559c12ba97ca458436
Yan-Fonseca <66083422+Yan-Fonseca@users.noreply.github.com>
2023-06-13 16:04:52 -0300

    Adição da estrutura do card de perfil para buscas e acessos na plataforma

---
+++ src/components/common/perfilCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="perfil-card">
+        <img src="../../assets/imgs/perfil.png" alt="foto de perfil">
+        <div class="data">
+            <p>Nome</p>
+            <button class="perfil-btn">Perfil</button>
+            <p>Avaliação: x.x/5.0</p>
+        </div>
+    </div>
+</template>
+
+
+
+<style scoped>
+    .perfil-card {
+        width: 187px;
+        height: 241px;
+        border-radius: 15px;
+        background-color: rgba(29, 36, 45, 0.84);
+        color: white;
+    }
+
+    .perfil-btn {
+        border-radius: 7px;
+        color:black;
+        background-color: rgba(56, 123, 223, 1);
+    }
+</style>
