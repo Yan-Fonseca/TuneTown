dec1f9598cd3a64ec58558441be56b4d239131a7
Yan-Fonseca <66083422+Yan-Fonseca@users.noreply.github.com>
2023-06-13 16:36:53 -0300

    Melhorando a disposição dos itens nos containers da Home e tentando ajeitar a largura dos mesmos

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,15 +1,37 @@
 <template>
     <div class="Home">
-        
+        <div class="avaliados">
+            <h1>Profissionais mais bem avaliados</h1>
+            <PerfilContainer/>
+        </div>
+        <div class="perto">
+            <h1>Profissionais perto de você!</h1>
+            <PerfilContainer/>
+        </div>
     </div>
 </template>
 
 <script>
+    import PerfilContainer from './PerfilContainer.vue';
+
     export default {
-        name: 'HomePage'
+        name: 'HomePage',
+        components: {
+            PerfilContainer
+        }
     }
 </script>
 
 <style scoped>
+    body {
+        background-color: black;
+    }
 
+    .avaliados, .perto {
+        min-width: 75%;
+        display: flex;
+        flex-direction: column; /* Alinha os itens verticalmente */
+        justify-content: center; /* Centraliza verticalmente */
+        align-items: center; /* Centraliza horizontalmente */
+    }
 </style>
---
+++ src/components/Home/PerfilContainer.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <PerfilCard/>
+        <PerfilCard/>
+        <PerfilCard/>
+        <PerfilCard/>
+    </div>
+</template>
+
+<script>
+    import PerfilCard from "../common/perfilCard.vue"
+
+    export default {
+        name: 'PerfilContainer',
+        components: {
+            PerfilCard
+        }
+    }
+</script>
+
+<style scoped>
+    .container {
+        background-color: rgba(42, 52, 62, 1);
+        border-radius: 20px;
+        display: flex; /* Adicionado para usar flexbox */
+        flex-direction: row; /* Alinha os itens verticalmente */
+        justify-content: center; /* Centraliza verticalmente */
+        align-items: center; /* Centraliza horizontalmente */
+
+        margin-bottom: 10px;
+        margin-top: 10px;
+    }
+</style>
