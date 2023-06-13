8bd83b1ce5fbb23780cdb788295bf32543451201
Yan-Fonseca <66083422+Yan-Fonseca@users.noreply.github.com>
2023-06-13 18:23:43 -0300

    adição do elemento de seleção de Estados

--- src/components/Busca/Filtro.vue
+++ src/components/Busca/Filtro.vue
@@ -1,9 +1,62 @@
 <template>
-    
+    <div class="filtro">
+        <h2>Filtrar Busca</h2>
+        <div class="info">
+            <form action="">
+                <div>
+                    <label for="selecao">Estado:</label>
+                    <select v-model="selectedOption" id="selecao">
+                        <option v-for="option in options" :value="option.value" :key="option.value">{{ option.label }}</option>
+                    </select>
+                </div>
+            </form>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'FiltroBusca'
+    name: 'FiltroBusca',
+    data() {
+        return {
+            selectedOption: null,
+            options: [
+            { value: 1, label: 'Acre' },
+            { value: 2, label: 'Alagoas' },
+            { value: 3, label: 'Amapá' },
+            { value: 4, label: 'Amazonas' },
+            { value: 5, label: 'Bahia' },
+            { value: 6, label: 'Ceará' },
+            { value: 7, label: 'Distrito Federal' },
+            { value: 8, label: 'Espírito Santo' },
+            { value: 9, label: 'Goiás' },
+            { value: 10, label: 'Maranhão' },
+            { value: 11, label: 'Mato Grosso' },
+            { value: 12, label: 'Mato Grosso do Sul' },
+            { value: 13, label: 'Minas Gerais' },
+            { value: 14, label: 'Pará' },
+            { value: 15, label: 'Paraíba' },
+            { value: 16, label: 'Paraná' },
+            { value: 17, label: 'Pernambuco' },
+            { value: 18, label: 'Piauí' },
+            { value: 19, label: 'Rio de Janeiro' },
+            { value: 20, label: 'Rio Grande do Norte' },
+            { value: 21, label: 'Rio Grande do Sul' },
+            { value: 22, label: 'Rondônia' },
+            { value: 23, label: 'Roraima' },
+            { value: 24, label: 'Santa Catarina' },
+            { value: 25, label: 'São Paulo' },
+            { value: 26, label: 'Sergipe' },
+            { value: 27, label: 'Tocantins' }
+            ]
+        };
+    }
 }
 </script>
+
+<style scoped>
+    .filtro {
+        background-color: rgba(29, 36, 45, 0.84);
+        color: white;
+    }
+</style>
--- src/views/BuscaView.vue
+++ src/views/BuscaView.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="container">
         <BuscaPerfil/>
+        <FiltroBusca/>
     </div>
 </template>
 
 <script>
     import BuscaPerfil from "../components/Busca/Busca.vue"
+    import FiltroBusca from "../components/Busca/Filtro.vue"
 
     export default {
         name: 'BuscaView',
         components: {
-            BuscaPerfil
+            BuscaPerfil,
+            FiltroBusca
         }
     }
 </script>
