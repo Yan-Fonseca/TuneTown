d35f00fb16af66653b461c4c0c086e5205bfb368
Yan-Fonseca <66083422+Yan-Fonseca@users.noreply.github.com>
2023-06-13 19:35:57 -0300

    protótipo de exibição dos resultados

--- src/components/Busca/Resultados.vue
+++ src/components/Busca/Resultados.vue
@@ -35,4 +35,10 @@ export default {
     h1 {
         font-size: 47px;
     }
+
+    .resultados {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        gap: 20px;
+    }
 </style>
