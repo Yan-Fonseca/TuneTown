4d6ca68afb543a8b88cf34a29686a1f732b76678
Joel-Henrique <joel.henrique@estudante.ufjf.br>
2023-06-15 20:14:49 -0300

    WelcomeView

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="corpo">
+    <div id="registro-container">
+        <WelcomeComp/>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import WelcomeComp from '@/components/Welcome/Welcome.vue'
+  
+  export default {
+    name: 'WelcomeView',
+    components: {
+        WelcomeComp
+    }
+  }
+  </script>
+  
+  <style scoped>
+    #registro-container {
+    margin-right: 50px;
+    background-color: rgba(0, 3, 22, 1);
+    background-color: transparent;
+   }
+  .corpo {
+    background-image: url("../assets/imgs/fundo_registro.png");
+    background-size: cover;
+    background-position: center;
+    min-height: 100vh;
+    margin: 0;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    background-repeat: no-repeat;
+  }
+  </style>
+  
