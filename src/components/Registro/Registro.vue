54c8b9e4ffd6337d983dde03cedc55a387036e55
Joel-Henrique <joel.henrique@estudante.ufjf.br>
2023-06-15 19:01:50 -0300

    Data Tema DarK

--- src/components/Registro/Registro.vue
+++ src/components/Registro/Registro.vue
@@ -38,7 +38,7 @@
         </div>
         <div class="column">
             <label for="dataNascimento">Data de Nascimento</label>
-            <VueDatePicker v-model="dataNascimento"></VueDatePicker>
+            <VueDatePicker v-model="dataNascimento" model-type="dd.MM.yyyy" :dark="true"></VueDatePicker>
           </div>
         </div>
         <div class="checkbox">
@@ -83,7 +83,7 @@
         nome: "",
         dataNascimento: null,
         cidade: "",
-        musico: false,
+        musico: false
       }
     },
     methods: {
@@ -104,6 +104,27 @@
 </script>
 
 <style scoped>
+.dp__theme_dark {
+   --dp-background-color: rgba(217, 217, 217, 0.47);
+   --dp-text-color: #ffffff;
+   --dp-hover-color: #484848;
+   --dp-hover-text-color: #ffffff;
+   --dp-hover-icon-color: #959595;
+   --dp-primary-color: #005cb2;
+   --dp-primary-text-color: #ffffff;
+   --dp-secondary-color: #a9a9a9;
+   --dp-border-color: #2d2d2d;
+   --dp-menu-border-color: #2d2d2d;
+   --dp-border-color-hover: #aaaeb7;
+   --dp-disabled-color: #737373;
+   --dp-scroll-bar-background: #212121;
+   --dp-scroll-bar-color: #484848;
+   --dp-success-color: #00701a;
+   --dp-success-color-disabled: #428f59;
+   --dp-icon-color: #959595;
+   --dp-danger-color: #e53935;
+   --dp-highlight-color: rgba(0, 92, 178, 0.2);
+}
 .text{
   font-size: 20px;
   margin-top: 10px;
