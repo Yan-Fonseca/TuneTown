9afe330fa75d014ded0dc12da8b7cd3b5480a95c
Yan-Fonseca <66083422+Yan-Fonseca@users.noreply.github.com>
2023-06-18 11:43:56 -0300

    Adicionando scrollbar na div resultados

--- src/components/Busca/Resultados.vue
+++ src/components/Busca/Resultados.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="container">
       <h1>Resultados:</h1>
-      <div class="resultados">
+      <div class="resultados-container">
+        <div class="resultados">
         <perfilCard v-for="item in users" :key="item.id" :nome="item.nome" :avaliacao="item.avaliacao" />
+      </div>
       </div>
     </div>
   </template>
@@ -25,7 +27,12 @@
           { id: 4, nome: 'Amanda', avaliacao: 9.9 },
           { id: 5, nome: 'Alexandre', avaliacao: 8.5 },
           { id: 6, nome: 'Julio', avaliacao: 7.8 },
-          { id: 7, nome: 'Cleopatra', avaliacao: 9.6 }
+          { id: 7, nome: 'Cleopatra', avaliacao: 9.6 },
+          { id: 0, nome: 'Yan', avaliacao: 9.9 },
+          { id: 1, nome: 'Joel', avaliacao: 8.5 },
+          { id: 2, nome: 'João', avaliacao: 7.8 },
+          { id: 3, nome: 'Gabriela', avaliacao: 9.6 },
+          { id: 4, nome: 'Amanda', avaliacao: 9.9 },
         ]
       };
     }
@@ -40,18 +47,23 @@
     border-radius: 20px;
     padding: 1rem;
     margin: 10px;
-    width: 90%; /* Ocupa 90% da largura disponível */
-    height: 100%; /* Ocupa toda a altura disponível */
+    width: 90%; 
+    height: 100%; 
   }
   
   h1 {
     font-size: 47px;
+  }
+
+  .resultados-container {
+    overflow-y: auto;
   }
   
   .resultados {
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     gap: 20px;
+    grid-auto-rows: 250px;
   }
   
   </style>
