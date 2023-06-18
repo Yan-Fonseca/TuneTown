f696e46bb3e556e093590f998ffca72243b4baa8
Yan-Fonseca <66083422+Yan-Fonseca@users.noreply.github.com>
2023-06-18 20:48:44 -0300

    calendário estilizado

--- src/components/Calendario/Calendario.vue
+++ src/components/Calendario/Calendario.vue
@@ -1,67 +1,61 @@
 <template>
     <div class="calendar">
-        <vue-cal class="cal" :time="false" active-view="month" events-on-month-view="short"/>
+      <vue-cal :time="false" active-view="month" events-on-month-view="short" :styles="calendarStyles" :event-styles="eventStyles"></vue-cal>
     </div>
-</template>
-
-<script>
-import VueCal from 'vue-cal'
-import 'vue-cal/dist/vuecal.css'
-
-export default {
-name:'CalendarioUser',
-components: {
-    VueCal
-},
-data() {
-    return {
-    events: [
-        {
-        title: 'Event 1',
-        start: '2023-06-15',
-        end: '2023-06-15'
-        },
-        {
-        title: 'Event 2',
-        start: '2023-06-17',
-        end: '2023-06-19'
-        }
-    ],
-    calendarStyles: {
-        calendar: {
-        background: '#f0f0f0',
-        color: '#333333'
-        },
-        header: {
-        background: '#dddddd',
-        color: '#333333',
-        fontWeight: 'bold'
-        },
-        event: {
-        background: '#ffcc00',
-        color: '#ffffff',
-        borderRadius: '4px'
-        }
+  </template>
+  
+  <script>
+  import VueCal from 'vue-cal';
+  import 'vue-cal/dist/vuecal.css';
+  
+  export default {
+    name: 'CalendarioUser',
+    components: {
+      VueCal
     },
-    eventStyles: {
-        backgroundColor: '#ffcc00',
-        color: '#ffffff',
-        borderRadius: '4px'
+    data() {
+      return {
+        events: [
+          {
+            title: 'Event 1',
+            start: '2023-06-15',
+            end: '2023-06-15'
+          },
+          {
+            title: 'Event 2',
+            start: '2023-06-17',
+            end: '2023-06-19'
+          }
+        ],
+        calendarStyles: {
+          calendar: {
+            background: '#ffffff',
+            color: '#333333'
+          },
+          header: {
+            background: '#f0f0f0',
+            color: '#333333',
+            fontWeight: 'bold'
+          },
+          event: {
+            background: '#f0f0f0',
+            color: '#333333',
+            borderRadius: '4px'
+          }
+        },
+        eventStyles: {
+          backgroundColor: '#f0f0f0',
+          color: '#333333',
+          borderRadius: '4px'
+        }
+      };
     }
-    };
-}
-};
-</script>
-
-<style scoped>
-    .calendar {
-        background-color: rgba(36, 43, 52, 1);
-        color: white;
-    }
-
-    .cal {
-        background-color: rgba(41, 51, 61, 1);
-        height: 100%;
-    }
-
-</style>
+  };
+  </script>
+  
+  <style scoped>
+  .calendar {
+    background-color: #ffffff;
+  }
+  </style>
+  
---
+++ src/components/Calendario/Evento.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Agenda'
+}
+</script>
+
+<style scoped>
+
+</style>
