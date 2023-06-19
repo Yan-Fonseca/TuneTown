5e27390760d072355e0af389bf5a2b190ab75bfa
Yan-Fonseca <66083422+Yan-Fonseca@users.noreply.github.com>
2023-06-19 09:03:37 -0300

    Adição do container de ano da aba de eventos

--- src/components/Calendario/Calendario.vue
+++ src/components/Calendario/Calendario.vue
@@ -1,68 +1,68 @@
 <template>
-    <div class="calendar">
-      <vue-cal
-        active-view="month"
-        style="height: 96vh"
-        :styles="calendarStyles"
-        :event-styles="eventStyles"
-        :cell-height="cellHeight"
-      ></vue-cal>
-    </div>
-  </template>
-  
-  <script>
-  import VueCal from 'vue-cal';
-  import 'vue-cal/dist/vuecal.css';
-  
-  export default {
-    name: 'CalendarioUser',
-    components: {
-      VueCal
-    },
-    data() {
-      return {
-        events: [
-          {
-            title: 'Event 1',
-            start: '2023-06-15',
-            end: '2023-06-15'
-          },
-          {
-            title: 'Event 2',
-            start: '2023-06-17',
-            end: '2023-06-19'
-          }
-        ],
-        calendarStyles: {
-          calendar: {
-            background: '#ffffff',
-            color: '#333333'
-          },
-          header: {
-            background: '#f0f0f0',
-            color: '#333333',
-            fontWeight: 'bold'
-          },
-          event: {
-            background: '#f0f0f0',
-            color: '#333333',
-            borderRadius: '4px'
-          }
+  <div class="calendar">
+    <vue-cal
+      active-view="month"
+      style="height: 96vh"
+      :styles="calendarStyles"
+      :event-styles="eventStyles"
+      :cell-height="cellHeight"
+    ></vue-cal>
+  </div>
+</template>
+
+<script>
+import VueCal from 'vue-cal';
+import 'vue-cal/dist/vuecal.css';
+
+export default {
+  name: 'CalendarioUser',
+  components: {
+    VueCal
+  },
+  data() {
+    return {
+      events: [
+        {
+          title: 'Event 1',
+          start: '2023-06-15',
+          end: '2023-06-15'
         },
-        eventStyles: {
-          backgroundColor: '#f0f0f0',
+        {
+          title: 'Event 2',
+          start: '2023-06-17',
+          end: '2023-06-19'
+        }
+      ],
+      calendarStyles: {
+        calendar: {
+          background: '#ffffff',
+          color: '#333333'
+        },
+        header: {
+          background: '#f0f0f0',
+          color: '#333333',
+          fontWeight: 'bold'
+        },
+        event: {
+          background: '#f0f0f0',
           color: '#333333',
           borderRadius: '4px'
-        },
-        cellHeight: 60
-      };
-    }
-  };
-  </script>
-  
-  <style scoped>
-  .calendar {
-    background-color: #ffffff;
+        }
+      },
+      eventStyles: {
+        backgroundColor: '#f0f0f0',
+        color: '#333333',
+        borderRadius: '4px'
+      },
+      cellHeight: 60
+    };
   }
-  </style>
+};
+</script>
+
+<style scoped>
+.calendar {
+  background-color: #ffffff;
+}
+</style>
   
--- src/components/Calendario/Evento.vue
+++ src/components/Calendario/Evento.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="container">
-        <h1 style="color: white;">Exemplo</h1>
+        <div class="ano">
+            <h1>2023</h1>
+        </div>
     </div>
 </template>
 
@@ -11,5 +13,26 @@ export default {
 </script>
 
 <style scoped>
+    .container {
+        background-color: rgba(36, 43, 52, 1);
+        color: white;
+    }
+
+    .ano {
+        background-color: rgba(41, 51, 61, 1);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        padding-bottom: 20px;
+        padding-top: 20px;
+
+        box-shadow: 0px 5px 4px rgba(0, 0, 0, 0.25);
+    }
+
+    h1 {
+        font-size: 40px;
+        align-items: center;
+    }
 
 </style>
