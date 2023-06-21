38f8f36efc2b3befad400dbe3494acfecdf23151
Yan-Fonseca <66083422+Yan-Fonseca@users.noreply.github.com>
2023-06-21 20:49:40 -0300

    Conseguindo estilizar calendário. Finalmentegit add .

--- src/components/Calendario/Calendario.vue
+++ src/components/Calendario/Calendario.vue
@@ -2,11 +2,9 @@
   <div class="calendar">
     <vue-cal
       active-view="month"
+      events-on-month-view="short"
+      :events="events"
       style="height: 93vh"
-      :event="events"
-      :styles="calendarStyles"
-      :event-styles="eventStyles"
-      :cell-height="cellHeight"
     ></vue-cal>
   </div>
 </template>
@@ -27,48 +25,24 @@ export default {
     return {
       events: [
         {
-          title: 'Event 1',
           start: '2023-06-15',
-          end: '2023-06-15'
-        },
-        {
-          title: 'Event 2',
-          start: '2023-06-17',
-          end: '2023-06-19'
+          end: '2023-06-15',
+          title: 'Event 1',
+          content: 'Teste',
+          class: 'music'
         }
-      ],
-      calendarStyles: {
-        calendar: {
-          background: '#ffffff',
-          color: '#333333'
-        },
-        header: {
-          background: '#f0f0f0',
-          color: '#333333',
-          fontWeight: 'bold'
-        },
-        event: {
-          background: '#f0f0f0',
-          color: '#333333',
-          borderRadius: '4px'
-        }
-      },
-      eventStyles: {
-        backgroundColor: '#f0f0f0',
-        color: '#333333',
-        borderRadius: '4px'
-      },
-      cellHeight: 60
+      ]
     };
   },
   methods: {
     tratarEventosParaCalendario() {
-      let data = this.props.data;
+      let data = this.data;
       data.forEach(element => {
         this.events.push({
           title: element.nome,
           start: element.data,
-          end: element.data
+          end: element.data,
+          content: element.descricao
         })
       });
     }
@@ -76,9 +50,12 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
 .calendar {
   background-color: #ffffff;
 }
+
+.vuecal__event.music {background-color: rgba(255, 102, 102, 0.9);border: 1px solid rgb(235, 82, 82);color: #fff;}
+
 </style>
   
