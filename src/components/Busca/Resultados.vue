0450b369128bdc7ec15761a170769945bd64d49d
Yan-Fonseca <66083422+Yan-Fonseca@users.noreply.github.com>
2023-06-21 13:57:50 -0300

    concluindo merge que faltava

--- src/components/Busca/Resultados.vue
+++ src/components/Busca/Resultados.vue
@@ -27,14 +27,6 @@
           { id: 4, nome: 'Amanda', avaliacao: 9.9 },
           { id: 5, nome: 'Alexandre', avaliacao: 3.0 },
           { id: 6, nome: 'Julio', avaliacao: 7.8 },
-<<<<<<< HEAD
-          { id: 7, nome: 'Cleopatra', avaliacao: 9.6 },
-          { id: 0, nome: 'Yan', avaliacao: 9.9 },
-          { id: 1, nome: 'Joel', avaliacao: 8.5 },
-          { id: 2, nome: 'João', avaliacao: 7.8 },
-          { id: 3, nome: 'Gabriela', avaliacao: 9.6 },
-          { id: 4, nome: 'Amanda', avaliacao: 9.9 },
-=======
           { id: 7, nome: 'Cleopatra', avaliacao: 2 },
           { id: 8, nome: 'Cleo', avaliacao: 0 },
           { id: 0, nome: 'Yan', avaliacao: 10.0 },
@@ -48,7 +40,6 @@
           { id: 2, nome: 'João', avaliacao: 1.8 },
           { id: 3, nome: 'Gabriela', avaliacao: 1.0 },
           { id: 4, nome: 'Amanda', avaliacao: 9.9 }
->>>>>>> thiago
         ]
       };
     }
@@ -62,15 +53,9 @@
     color: white;
     border-radius: 15px;
     padding: 1rem;
-<<<<<<< HEAD
-    margin: 10px;
-    width: 90%; 
-    height: 100%; 
-=======
     margin: 7px;
-    width: 95%; /* Ocupa 90% da largura disponível */
-    height: 100%; /* Ocupa toda a altura disponível */
->>>>>>> thiago
+    width: 95%; 
+    height: 100%;
   }
   
   h1 {
