7f0d4807671d01e319785002e9bd952638b24dd3
Thiago Lopes <tiagojosecnn@outlook.com>
2023-06-25 19:46:18 -0300

    Adiciona estilo a firstPage.

--- src/views/FirstPageView.vue
+++ src/views/FirstPageView.vue
@@ -11,6 +11,25 @@
 		name: 'FirstPageView',
 		components: {
 			FirstPage
-		}
-	}
+		},
+} 
 </script>
+
+<style scoped>
+  .first {
+    background-image: url("../assets/imgs/fundo_login.png");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+  }
+  * {
+    font-family: 'Arapey', serif;
+  }
+ 
+  
+  </style>
