4ac5a9f50c25fddaa7cc7df72a996e503ed82688
Thiago Lopes <tiagojosecnn@outlook.com>
2023-06-25 13:41:17 -0300

    Adiciona componentes view.

---
+++ src/views/FirstPageView.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="first">
+		<FirstPage/>
+	</div>
+</template>
+
+<script>
+	import FirstPage from '@/components/FirstPage/FirstPage.vue';
+
+	export default {
+		name: 'FirstPageView',
+		components: {
+			FirstPage
+		}
+	}
+</script>
