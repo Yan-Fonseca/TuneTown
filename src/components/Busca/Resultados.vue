db7b53b6f9cc05eb3a3ae37fb12a9f82cb2cad02
Alfredo Lucas <alfredoliu14@live.com>
2023-06-25 11:50:14 -0300

    corrige busca usuario

--- src/components/Busca/Resultados.vue
+++ src/components/Busca/Resultados.vue
@@ -3,69 +3,66 @@
     <h1>Resultados:</h1>
     <div class="resultados-container">
       <div class="resultados">
-      <perfilCard v-for="item in users" :key="item.id" :nome="item.nome" :avaliacao="item.avaliacao" />
-    </div>
+        <perfilCard
+          v-for="item in users"
+          :key="item.id"
+          :nome="item.nome"
+          :avaliacao="item.avaliacao"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  import { fetchData } from '@/firebase';
-  import perfilCard from '../common/perfilCard.vue';
+import { fetchData } from "@/firebase";
+import perfilCard from "../common/perfilCard.vue";
 
-  export default {
-    name: 'ResultadosBusca',
-    components: {
-      perfilCard
-    },
-    data() {
-      return {
-        users: [
-          { id: 0, nome: 'Yan', avaliacao: 5.0 },
-          { id: 1, nome: 'Joel', avaliacao: 5.5 },
-          { id: 2, nome: 'João', avaliacao: 1.8 },
-          { id: 3, nome: 'Gabriela', avaliacao: 1.0 },
-          { id: 4, nome: 'Amanda', avaliacao: 9.9 },
-          { id: 5, nome: 'Alexandre', avaliacao: 3.0 },
-          { id: 6, nome: 'Julio', avaliacao: 7.8 },
-          { id: 7, nome: 'Cleopatra', avaliacao: 2 },
-          { id: 8, nome: 'Cleo', avaliacao: 0 }
-        ],
-        counter: 9
-      };
-    },
-    methods: {
-      async buscarUsuarios(dados) {
-        try {
-          const usersDataPromise = fetchData(dados);
-          const usersData = await usersDataPromise;
-          if (usersData) {
-            this.users = [];
-            const user = usersData[0]; // Obter o primeiro usuário do array
+export default {
+  name: "ResultadosBusca",
+  components: {
+    perfilCard,
+  },
+  data() {
+    return {
+      users: [],
+      counter: 0,
+    };
+  },
+  methods: {
+    async buscarUsuarios(dados) {
+      try {
+        const usersDataPromise = fetchData(dados);
+        const usersData = await usersDataPromise;
+        if (usersData) {
+          this.users = [];
+          for (var i = 0; i < usersData.length; i++) {
+            const user = usersData[i]
             const data = {
               id: this.counter,
               nome: user.nome,
               avaliacao: 5,
             };
+            this.counter++;
             this.users.push(data);
           }
-        } catch (error) {
-          console.log('erro: ' + error);
         }
-      },
+      } catch (error) {
+        console.log("erro: " + error);
+      }
     },
-  }
+  },
+};
 </script>
 
 <style scoped>
-
 .container {
   background-color: rgba(29, 36, 45, 0.84);
   color: white;
   border-radius: 15px;
   padding: 1rem;
   margin: 7px;
-  width: 95%; 
+  width: 95%;
   height: 100%;
 }
 
@@ -83,5 +80,4 @@ h1 {
   gap: 20px;
   grid-auto-rows: 250px;
 }
-
 </style>
